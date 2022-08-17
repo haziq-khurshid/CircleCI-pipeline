cd3aa3f0b885d53c92165d915cf8a2bfb5ffa238
Haziq Khurshid <haziq.khurshid@gmail.com>
2022-08-17 13:46:15 +0500

    updated tests

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from main import Add
 
 def TestAdd():
     assert Add(2,3) == 5
-    assert Add(5,5) == 10
+    assert Add(15,5) == 20
     print("Add function works correctly")
 
 if __name__ == '__main__':
